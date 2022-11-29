7b50e1bcd9bd06f25d0ada4b1570369ed890e747
Nicolas Cicoira <114522266+tasto-di-espulsione@users.noreply.github.com>
2022-11-29 21:08:06 +0100

    Update TcpServer.java

--- Esercizio a/TcpServer.java
+++ Esercizio a/TcpServer.java
@@ -45,7 +45,7 @@ public class TcpServer {
 				//clientMsg += "Connection: close\r\n";
 				//clientMsg += "Content-Type: text/plain\r\n";
 				clientMsg += "\r\n";
-				clientMsg += "Saluti dal web server Java";
+				clientMsg += "Nicolas Cicoira";
 				outStream.write(clientMsg.getBytes());
 				outStream.flush();
 
